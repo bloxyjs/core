db11dac5486a36e04d7998e1321dc6e4c2beb99d
Sebastian Erik Bauer <dataisloading@outlook.com>
2022-01-23 02:41:47 +0100

    chore: remove unused files

--- src/structures/index.ts
+++ src/structures/index.ts
@@ -1,13 +1,13 @@
 export * from "./ChatConversation/BaseChatConversation";
 export * from "./ChatConversation/ChatConversation";
 
+export * from "./User/BaseUser";
+export * from "./User/User";
+
 export * from "./Group/BaseGroup";
 export * from "./Group/Group";
 
 export * from "./GroupMember/BaseGroupMember";
 export * from "./GroupMember/GroupMember";
-
-export * from "./User/BaseUser";
-export * from "./User/User";
 
 export * from "./CursorPage";
